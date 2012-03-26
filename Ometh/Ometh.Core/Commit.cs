fdca5073485d597082842f40035cb4a97f3ba602
Dennis Daume <daume.dennis@gmail.com>
2012-03-26 02:02:22 +0200

    Added a few classes to get used to the NGit API

---
+++ Ometh/Ometh.Core/Commit.cs
@@ -0,0 +1,12 @@
+﻿namespace Ometh.Core
+{
+    public class Commit
+    {
+        public string Message { get; private set; }
+
+        public Commit(string message)
+        {
+            this.Message = message;
+        }
+    }
+}
---
+++ Ometh/Ometh.Core/Repository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NGit.Api;
+using NGit.Storage.File;
+
+namespace Ometh.Core
+{
+    public class Repository
+    {
+        private readonly Git git;
+
+        public Repository(string path)
+        {
+            if (path == null)
+                throw new ArgumentNullException("path");
+
+            this.git = new Git(new FileRepository(path));
+        }
+
+        public IEnumerable<Commit> Log()
+        {
+            return this.git
+                .Log()
+                .Call()
+                .Select(commit => new Commit(commit.GetFullMessage()));
+        }
+    }
+}
